f701443fc67e5b5ffe505869d7fa0d6d21718bd5
owett <47257871+owett@users.noreply.github.com>
2019-07-05 20:37:21 +0300

    Update Unit3.pas

--- src/Project3/Unit3.pas
+++ src/Project3/Unit3.pas
@@ -10,6 +10,12 @@ type
   TForm3 = class(TForm)
     Button1: TButton;
     Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
     procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
@@ -19,6 +25,7 @@ type
 
 var
   Form3: TForm3;
+  cell,x,y:integer;
 
 implementation
 
@@ -27,15 +34,18 @@ implementation
 procedure TForm3.Button1Click(Sender: TObject);
 var i,g:integer;
 begin
- for i := 0 to 16 do
+cell:=StrToInt(Edit1.Text);
+x:=StrToInt(Edit3.Text);
+y:=StrToInt(Edit2.Text);
+for i:= 0 to x do
  begin
-  image1.Canvas.MoveTo(i*15,0);
-  image1.Canvas.LineTo(i*15,120);
+  image1.Canvas.MoveTo(i*cell,0);
+  image1.Canvas.LineTo(i*cell,625);
  end;
- for g := 0 to 8 do
+ for g := 0 to y do
  begin
-  image1.Canvas.MoveTo(0,g*15);
-  image1.Canvas.LineTo(240,g*15);
+  image1.Canvas.MoveTo(0,g*cell);
+  image1.Canvas.LineTo(1078,g*cell);
  end;
 end;
 
